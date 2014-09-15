21694aa0e54399ef71d6ed3c0a0f2078387b15ea
Sam Morley 40901 <40901@longroad.ac.uk>
2014-09-15 12:18:58 +0100

    todays work

---
+++ first name.py
@@ -0,0 +1,5 @@
+print = (" what is your name?" )
+
+print = first_name  {}
+
+
---
+++ revision excersize.py
@@ -0,0 +1,14 @@
+print("hello, this programme is going to add together four integers for you")
+number_one = int(input("Please enter your first number: "))
+
+number_two = int(input("Please enter your second number: "))
+
+number_three = int(input("Please enter your third number: "))
+
+number_four = int(input("Please enter your fourth and final number: "))
+
+number_answer = number_one + number_two + number_three + number_four
+
+print(number_answer)
+
+print("thank you for using this program, created by sam morley 15/9/2014")
